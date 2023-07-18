3c5cb13d023de78ea741a903515dcbb1cd10ce58
Nanthakumar Sathiyanandham <118247818+nanthu1696@users.noreply.github.com>
2023-07-18 17:28:07 +0530

    Update Posts.js

--- src/routes/before/Posts.js
+++ src/routes/before/Posts.js
@@ -7,28 +7,30 @@ export default function Posts() {
   const { posts } = useLoaderData();
   return (
     <div id="postsContainer">
-      <div id="postheading">Popular Posts</div>
-      <div id="togglebar">
-        <Link to={"/hot"} className="hot">
-          <box-icon type="solid" name="flame"></box-icon>
-          <span>Hot</span>
-        </Link>
-        <Link to={"/new"} className="hot">
-          <box-icon name="loader-circle"></box-icon>
-          <span>New</span>
-        </Link>
-        <Link to={"/top"} className="hot">
-          <box-icon name="bar-chart-alt-2"></box-icon>
-          <span>Top</span>
-        </Link>
+      <div>
+        <div id="postheading">Popular Posts</div>
+        <div id="togglebar">
+          <Link to={"/hot"} className="hot">
+            <box-icon type="solid" name="flame"></box-icon>
+            <span>Hot</span>
+          </Link>
+          <Link to={"/new"} className="hot">
+            <box-icon name="loader-circle"></box-icon>
+            <span>New</span>
+          </Link>
+          <Link to={"/top"} className="hot">
+            <box-icon name="bar-chart-alt-2"></box-icon>
+            <span>Top</span>
+          </Link>
+        </div>
+        {posts.length ? (
+          posts.map((post) => {
+            return <Post data={post} />;
+          })
+        ) : (
+          <Loading />
+        )}
       </div>
-      {posts.length ? (
-        posts.map((post) => {
-          return <Post data={post} />;
-        })
-      ) : (
-        <Loading />
-      )}
     </div>
   );
 }
